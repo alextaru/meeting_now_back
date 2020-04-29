7c62bd65689107d0624e124d4ecd47e42db545fc
alexandre gonçalves <alex.taru@yahoo.com.br>
2020-04-29 19:08:07 -0300

    feat: comparacao data

--- Controllers/AgendaController.cs
+++ Controllers/AgendaController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
+using System;
+using System.Globalization;
 
 namespace MeetingNow.Controllers{
     
@@ -21,14 +23,60 @@ namespace MeetingNow.Controllers{
 
         [HttpPost]
         [Route("")]
-        public async Task<ActionResult<Agenda>> Post(
+        public async Task<ActionResult<List<Agenda>>> Post(
             [FromServices] DataContext context,
             [FromBody] Agenda model
         ){
             if(ModelState.IsValid){
-                context.Agendas.Add(model);
-                await context.SaveChangesAsync();
-                return model;
+
+                string format = "yyyy-MM-dd hh:mm";
+                CultureInfo provider = CultureInfo.InvariantCulture;
+                
+                DateTime dataInit = DateTime.ParseExact(model.DataInit, format, provider);
+                DateTime dataEnd = DateTime.ParseExact(model.DataEnd, format, provider);
+
+
+                int result = DateTime.Compare(dataEnd, dataInit);
+
+                if(result < 0 || result == 0){
+                    return BadRequest("data invalida");
+                }else{
+
+                    var agenda = await context.Agendas.Where(x =>x.Sala == model.Sala).ToListAsync();
+
+                    bool valido = true;
+
+                    foreach (Agenda item in agenda){
+                        DateTime init = DateTime.ParseExact(item.DataInit, format, provider);
+                        DateTime end = DateTime.ParseExact(item.DataEnd, format, provider);
+
+                        int resultInit = DateTime.Compare(dataInit, init);
+                        int resultEnd = DateTime.Compare(dataInit, end);
+
+                        if((resultInit == 0) || (resultInit > 0 && resultEnd < 0) ){
+                            valido = false;
+                        }
+
+                        resultInit = DateTime.Compare(dataEnd, init);
+                        resultEnd = DateTime.Compare(dataEnd, end);
+
+                        if( resultEnd == 0 || (resultInit > 0 && resultEnd < 0) ){
+                            valido = false;
+                        }
+                        
+                    }
+
+                    if(valido){
+                        context.Agendas.Add(model);
+                        await context.SaveChangesAsync();
+                        agenda.Add(model);
+                        return agenda;  
+                    }else{
+                        return BadRequest("data invalida");
+                    }
+                                     
+                }
+                
             }else{
                 return BadRequest(ModelState);
             }
